bc6255e2b2b5022c9e18bd6ba69cf46494af7013
Vanilla <neko@hydev.org>
2021-09-19 18:33:04 +0800

    [+] deploy/server.sh for serve on NixOS.

---
+++ deploy/default.nix
@@ -0,0 +1,19 @@
+# https://nathan.gs/2019/04/19/using-jekyll-and-nix-to-blog/
+with import <nixpkgs> { };
+
+let jekyll_env = bundlerEnv rec {
+  name = "jekyll_env";
+  inherit ruby;
+
+  gemfile = ./Gemfile;
+  lockfile = ./Gemfile.lock;
+  gemset = ./gemset.nix;
+};
+in
+stdenv.mkDerivation rec {
+  name = "vergedx.github.io";
+  buildInputs = [ jekyll_env bundler ruby ];
+
+  # https://github.com/kaeyleo/jekyll-theme-H2O#:~:text=jekyll%20server
+  shellHook = "exec ${jekyll_env}/bin/jekyll server -s ../";
+}
